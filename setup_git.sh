e024547428da222df0a16fbd72c46d3f6749ef59
Yair Smadar <yair.smadar@synaptics.com>
2023-05-10 13:45:56 +0300

    add setup_git.sh

---
+++ setup_git.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Configure Git
+git config --global user.name "YairSmadar"
+git config --global user.email "yairsemama@gmail.com"
+
+# Generate SSH key (if it doesn't exist)
+if [ ! -f ~/.ssh/id_ed25519 ]; then
+  ssh-keygen -t ed25519 -C "yairsemama@gmail.com" -f /home/yair_smadar1/.ssh/id_ed25519 -N ""
+fi
+
+# Add the SSH key to the ssh-agent
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_ed25519
