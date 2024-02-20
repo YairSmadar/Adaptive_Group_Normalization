1f1dacba976b06e583e3f639a243052699098968
YairSmadar <yairsemama@gmail.com>
2024-02-20 13:07:33 +0200

    fix bug

--- classification/train.py
+++ classification/train.py
@@ -28,7 +28,7 @@ import os
 import sys
 
 # Add the project's root directory to sys.path
-project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "../"))
 sys.path.append(project_root)
 
 from agn_src.agn_scheduler import AGNScheduler
