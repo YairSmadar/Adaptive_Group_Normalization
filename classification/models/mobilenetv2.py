bdbc5484ce11ecc7ee4d3f514cfdb5734322d43a
YairSmadar <yairsemama@gmail.com>
2024-02-21 16:00:02 +0200

    fix mobilenet

--- classification/models/mobilenetv2.py
+++ classification/models/mobilenetv2.py
@@ -1,28 +1,78 @@
-import torch.nn as nn
-import math
+# MobileNetV2 is adopted from torchvision
+# (https://github.com/pytorch/vision/blob/master/torchvision/models/mobilenet.py)
+# Modified block decoder is adopted from official code of EfficientNet
+# (https://github.com/tensorflow/tpu/blob/master/models/official/efficientnet/efficientnet_builder.py)
+
+import re
+from torch import nn
 from agn_src.normalization import NormalizationFactory
 from agn_src.random_group_normalization import RandomGroupNorm as rgn
 from agn_src.similarity_group_normalization import SimilarityGroupNorm as sgn
 
-def conv_bn(inp, oup, stride, normalization_factory):
-    return nn.Sequential(
-        nn.Conv2d(inp, oup, 3, stride, 1, bias=False),
-        normalization_factory.create_norm2d(oup),
-        nn.ReLU6(inplace=True)
-    )
+class BlockDecoder(object):
+    """Block Decoder for readability."""
+
+    def _decode_block_string(self, block_string):
+        """Gets a block through a string notation of arguments."""
+        assert isinstance(block_string, str)
+        ops = block_string.split('_')
+        options = {}
+        for op in ops:
+            splits = re.split(r'(\d.*)', op)
+            if len(splits) >= 2:
+                key, value = splits[:2]
+                options[key] = value
+
+        if 's' not in options or len(options['s']) != 1:
+            raise ValueError('Strides options should be an integer.')
+
+        if 'wm' in options:
+            # width_mult, round_nearest, stride of stem
+            return [float(options['wm']), int(options['rn']), int(options['s'])]
+        return [int(options['t']), int(options['c']), int(options['n']), int(options['s'])]
+
+    def decode(self, string_list):
+        """Decodes a list of string notations to specify blocks inside the network.
+        Args:
+          string_list: a list of strings, each string is a notation of block.
+        Returns:
+          A list of namedtuples to represent blocks arguments.
+        """
+        assert isinstance(string_list, list)
+        blocks_args = []
+        for block_string in string_list:
+            blocks_args.append(self._decode_block_string(block_string))
+        return blocks_args
 
 
-def conv_1x1_bn(inp, oup, normalization_factory):
-    return nn.Sequential(
-        nn.Conv2d(inp, oup, 1, 1, 0, bias=False),
-        normalization_factory.create_norm2d(oup),
-        nn.ReLU6(inplace=True)
-    )
+def _make_divisible(v, divisor, min_value=None):
+    """
+    This function is taken from the original tf repo.
+    It ensures that all layers have a channel number that is divisible by 8
+    It can be seen here:
+    https://github.com/tensorflow/models/blob/master/research/slim/nets/mobilenet/mobilenet.py
+    :param v:
+    :param divisor:
+    :param min_value:
+    :return:
+    """
+    if min_value is None:
+        min_value = divisor
+    new_v = max(min_value, int(v + divisor / 2) // divisor * divisor)
+    # Make sure that round down does not go down by more than 10%.
+    if new_v < 0.9 * v:
+        new_v += divisor
+    return new_v
 
 
-def make_divisible(x, divisible_by=8):
-    import numpy as np
-    return int(np.ceil(x * 1. / divisible_by) * divisible_by)
+class ConvBNReLU(nn.Sequential):
+    def __init__(self, in_planes, out_planes, kernel_size=3, stride=1, groups=1, normalization_factory=None):
+        padding = (kernel_size - 1) // 2
+        super(ConvBNReLU, self).__init__(
+            nn.Conv2d(in_planes, out_planes, kernel_size, stride, padding, groups=groups, bias=False),
+            normalization_factory.create_norm2d(out_planes),
+            nn.ReLU6(inplace=True)
+        )
 
 
 class InvertedResidual(nn.Module):
@@ -31,33 +81,21 @@ class InvertedResidual(nn.Module):
         self.stride = stride
         assert stride in [1, 2]
 
-        hidden_dim = int(inp * expand_ratio)
+        hidden_dim = int(round(inp * expand_ratio))
         self.use_res_connect = self.stride == 1 and inp == oup
 
-        if expand_ratio == 1:
-            self.conv = nn.Sequential(
-                # dw
-                nn.Conv2d(hidden_dim, hidden_dim, 3, stride, 1, groups=hidden_dim, bias=False),
-                normalization_factory.create_norm2d(hidden_dim),
-                nn.ReLU6(inplace=True),
-                # pw-linear
-                nn.Conv2d(hidden_dim, oup, 1, 1, 0, bias=False),
-                normalization_factory.create_norm2d(oup),
-            )
-        else:
-            self.conv = nn.Sequential(
-                # pw
-                nn.Conv2d(inp, hidden_dim, 1, 1, 0, bias=False),
-                normalization_factory.create_norm2d(hidden_dim),
-                nn.ReLU6(inplace=True),
-                # dw
-                nn.Conv2d(hidden_dim, hidden_dim, 3, stride, 1, groups=hidden_dim, bias=False),
-                normalization_factory.create_norm2d(hidden_dim),
-                nn.ReLU6(inplace=True),
-                # pw-linear
-                nn.Conv2d(hidden_dim, oup, 1, 1, 0, bias=False),
-                normalization_factory.create_norm2d(oup),
-            )
+        layers = []
+        if expand_ratio != 1:
+            # pw
+            layers.append(ConvBNReLU(inp, hidden_dim, kernel_size=1, normalization_factory=normalization_factory))
+        layers.extend([
+            # dw
+            ConvBNReLU(hidden_dim, hidden_dim, stride=stride, groups=hidden_dim, normalization_factory=normalization_factory),
+            # pw-linear
+            nn.Conv2d(hidden_dim, oup, 1, 1, 0, bias=False),
+            normalization_factory.create_norm2d(oup),
+        ])
+        self.conv = nn.Sequential(*layers)
 
     def forward(self, x):
         if self.use_res_connect:
@@ -67,74 +105,94 @@ class InvertedResidual(nn.Module):
 
 
 class MobileNetV2(nn.Module):
-    def __init__(self, normalization_args: dict, n_class=100, input_size=32, width_mult=1):
+    def __init__(self,
+                 normalization_args,
+                 num_classes=1000,
+                 block_args=None,
+                 width_mult=1.0,
+                 inverted_residual_setting=None,
+                 round_nearest=8,
+                 stem_stride=2,
+                 block=None):
+        """
+        MobileNet V2 main class
+        Args:
+            num_classes (int): Number of classes
+            width_mult (float): Width multiplier - adjusts number of channels in each layer by this amount
+            inverted_residual_setting: Network structure
+            round_nearest (int): Round the number of channels in each layer to be a multiple of this number
+            Set to 1 to turn off rounding
+            block: Module specifying inverted residual building block for mobilenet
+        """
         super(MobileNetV2, self).__init__()
-        block = InvertedResidual
+
+        if block is None:
+            block = InvertedResidual
         input_channel = 32
         last_channel = 1280
-        interverted_residual_setting = [
-            # t, c, n, s
-            [1, 16, 1, 1],
-            [6, 24, 2, 2],
-            [6, 32, 3, 2],
-            [6, 64, 4, 2],
-            [6, 96, 3, 1],
-            # [6, 160, 3, 2],
-            # [6, 320, 1, 1],
-        ]
 
-        if input_size > 32:
-            interverted_residual_setting.append([6, 160, 3, 2])
-            interverted_residual_setting.append([6, 320, 1, 1])
+        if inverted_residual_setting is None:
+            inverted_residual_setting = [
+                # t, c, n, s
+                [1, 16, 1, 1],
+                [6, 24, 2, 2],
+                [6, 32, 3, 2],
+                [6, 64, 4, 2],
+                [6, 96, 3, 1],
+                [6, 160, 3, 2],
+                [6, 320, 1, 1],
+            ]
+        if block_args is not None:
+            decoder = BlockDecoder()
+            block_args = decoder.decode(block_args)
+            if len(block_args[0]) == 3:
+                width_mult, round_nearest, stem_stride = block_args[0]
+            inverted_residual_setting = [block_arg for block_arg in block_args if len(block_arg) > 3]
 
         self.normalization_factory = NormalizationFactory(normalization_args['version'],
                                                           **normalization_args['norm_factory_args'],
                                                           **normalization_args['SGN_args'])
 
+        # only check the first element, assuming user knows t,c,n,s are required
+        if len(inverted_residual_setting) == 0 or len(inverted_residual_setting[0]) != 4:
+            raise ValueError("inverted_residual_setting should be non-empty "
+                             "or a 4-element list, got {}".format(inverted_residual_setting))
+
         # building first layer
-        assert input_size % 32 == 0
-        # input_channel = make_divisible(input_channel * width_mult)  # first channel is always 32!
-        self.last_channel = make_divisible(last_channel * width_mult) if width_mult > 1.0 else last_channel
-        self.features = [conv_bn(3, input_channel, 2, self.normalization_factory)]
+        input_channel = _make_divisible(input_channel * width_mult, round_nearest)
+        self.last_channel = _make_divisible(last_channel * max(1.0, width_mult), round_nearest)
+        features = [ConvBNReLU(3, input_channel, stride=stem_stride, normalization_factory=self.normalization_factory)]
         # building inverted residual blocks
-        for t, c, n, s in interverted_residual_setting:
-            output_channel = make_divisible(c * width_mult) if t > 1 else c
+        for t, c, n, s in inverted_residual_setting:
+            output_channel = _make_divisible(c * width_mult, round_nearest)
             for i in range(n):
-                if i == 0:
-                    self.features.append(block(input_channel, output_channel, s, expand_ratio=t, normalization_factory=self.normalization_factory))
-                else:
-                    self.features.append(block(input_channel, output_channel, 1, expand_ratio=t, normalization_factory=self.normalization_factory))
+                stride = s if i == 0 else 1
+                features.append(block(input_channel, output_channel, stride, expand_ratio=t, normalization_factory=self.normalization_factory))
                 input_channel = output_channel
         # building last several layers
-        self.features.append(conv_1x1_bn(input_channel, self.last_channel, normalization_factory=self.normalization_factory))
+        features.append(ConvBNReLU(input_channel, self.last_channel, kernel_size=1, normalization_factory=self.normalization_factory))
         # make it nn.Sequential
-        self.features = nn.Sequential(*self.features)
+        self.features = nn.Sequential(*features)
 
         # building classifier
-        self.classifier = nn.Linear(self.last_channel, n_class)
+        self.classifier = nn.Sequential(
+            nn.Dropout(0.2),
+            nn.Linear(self.last_channel, num_classes),
+        )
 
-        self._initialize_weights()
-
-    def forward(self, x):
-        x = self.features(x)
-        x = x.mean(3).mean(2)
-        x = self.classifier(x)
-        return x
-
-    def _initialize_weights(self):
+        # weight initialization
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
-                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-                m.weight.data.normal_(0, math.sqrt(2. / n))
+                nn.init.kaiming_normal_(m.weight, mode='fan_out')
                 if m.bias is not None:
-                    m.bias.data.zero_()
+                    nn.init.zeros_(m.bias)
             elif isinstance(m, nn.BatchNorm2d):
-                m.weight.data.fill_(1)
-                m.bias.data.zero_()
+                nn.init.ones_(m.weight)
+                nn.init.zeros_(m.bias)
             elif isinstance(m, nn.Linear):
-                n = m.weight.size(1)
-                m.weight.data.normal_(0, 0.01)
-                m.bias.data.zero_()
+                nn.init.normal_(m.weight, 0, 0.01)
+                if m.bias is not None:
+                    nn.init.zeros_(m.bias)
             elif isinstance(m, nn.GroupNorm):
                 m.weight.data.fill_(1)
                 m.bias.data.zero_()
@@ -145,17 +203,31 @@ class MobileNetV2(nn.Module):
                 m.groupNorm.weight.data.fill_(1)
                 m.groupNorm.bias.data.zero_()
 
+    def _forward_impl(self, x):
+        # This exists since TorchScript doesn't support inheritance, so the superclass method
+        # (this one) needs to have a name other than `forward` that can be accessed in a subclass
+        x = self.features(x)
+        # Cannot use "squeeze" as batch-size can be 1 => must use reshape with x.shape[0]
+        x = nn.functional.adaptive_avg_pool2d(x, 1).reshape(x.shape[0], -1)
+        x = self.classifier(x)
+        return x
 
-def mobilenet_v2(args, pretrained=False):
-    model = MobileNetV2(normalization_args=args["normalization_args"], n_class=args["n_class"],
-                        input_size=args["input_size"])
+    def forward(self, x):
+        return self._forward_impl(x)
 
-    if pretrained:
-        try:
-            from torch.hub import load_state_dict_from_url
-        except ImportError:
-            from torch.utils.model_zoo import load_url as load_state_dict_from_url
-        state_dict = load_state_dict_from_url(
-            'https://www.dropbox.com/s/47tyzpofuuyyv1b/mobilenetv2_1.0-f2a8633.pth.tar?dl=1', progress=True)
-        model.load_state_dict(state_dict)
+
+def mobilenet_v2(args):
+    inverted_residual_setting = [
+        # t, c, n, s
+        [1, 16, 1, 1],
+        [6, 24, 2, 1],
+        [6, 32, 3, 2],
+        [6, 64, 4, 2],
+        [6, 96, 3, 1],
+        [6, 160, 3, 2],
+        [6, 320, 1, 1],
+    ]
+
+    model = MobileNetV2(normalization_args=args["normalization_args"], num_classes=args["n_class"],
+                        inverted_residual_setting=inverted_residual_setting)
     return model
