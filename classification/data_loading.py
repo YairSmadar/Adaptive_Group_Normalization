a123cccca6d1b7e1511b53af1b8d4b381e9061a5
YairSmadar <yairsemama@gmail.com>
2024-03-26 14:27:54 +0200

    update imagenet dataloders

--- classification/data_loading.py
+++ classification/data_loading.py
@@ -112,8 +112,8 @@ def getLoaders(datasetName, gen, input_size=None):
             train_transform = transforms.Compose([
                 transforms.RandomResizedCrop(img_size),  # Tiny ImageNet images are 64x64
                 transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
                 transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-                transforms.ToTensor()
             ])
 
             val_transform = transforms.Compose([
