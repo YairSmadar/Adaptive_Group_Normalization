1038e4ec6993d093825948b408fc680e3a0e77dc
YairSmadar <yairsemama@gmail.com>
2024-03-26 12:10:00 +0200

    update download coco script

--- yolov5/data/download_coco.py
+++ yolov5/data/download_coco.py
@@ -1,4 +1,11 @@
-from yolov5.utils.general import download, Path
+import os
+import sys
+
+# Add the project's root directory to sys.path
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.append(project_root)
+
+from utils.general import download, Path
 import zipfile
 import os
 
